e31fe67f47e8257a2d70cf580a4253eabf2cadde
dode <dode>
2022-02-22 09:04:19 -0800

    Created function and vars

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,6 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //This is the UI var related for the displayed value
+    @IBOutlet weak var displayedTestLabel: UILabel!
+    
+    //This is the var responsible for currentTextView
+    var currentTextThatShouldBeDisplayed:String = ""
+    
+    //This is the var responsible for lastText
+    var lastTextView:String = ""
+    
+    //This is the var responsible for keep the last pushed sign
+    var lastPressedSign:String = ""
+    
+    
+    //This is the function for write the currentValue to display
+    func displayValue(value:String)
+    {
+        if lastPressedSign != "."{
+            if currentTextThatShouldBeDisplayed == "0"{
+                currentTextThatShouldBeDisplayed = ""
+            }
+            
+            
+            
+        }
+    }
+    
+    
+    //This is the function for the buttons. All buttons have reference to buttonActioner
+    @IBAction func buttonActioner(_ sender: UIButton) {
+        
+        //get the current value from sender
+        let currentValue:String = sender.currentTitle!
+        
+        displayValue(value: currentValue)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
